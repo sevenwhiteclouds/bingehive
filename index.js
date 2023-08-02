4695d7b693ea57811ac06d2c5d75ad1ba0beebc8
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-08-02 08:58:46 -0700

    db access success

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const pool = require("./dbPool.js");
 
+testDb();
+
+async function testDb() {
+  console.log(await executeSQL("SELECT * FROM user"));
+  console.log(await executeSQL("SELECT * FROM list"));
+  console.log(await executeSQL("SELECT * FROM list_entry"));
+}
+
 async function executeSQL(query, params) {
   return new Promise((resolve, reject) => {
     pool.query(query, params, (err, rows, fields) => {
