02c13da7756331e89d2164b017a769c261bed00b
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-08-02 09:50:51 -0700

    tracking with repl project

--- src/index.js
+++ src/index.js
@@ -7,21 +7,33 @@ const session = require('express-session');
 const bcrypt = require('bcrypt');
 
 app.use(express.static("public"));
-app.use(express.static("public"));
 app.set("view engine", "ejs");
+
 app.use(session ({
-  secret: "top secret!",
+  secret: "top secret!", 
   resave: true,
   saveUninitialized: true
 }));
 
-app.use(express.urlencoded({extended:true})); //to be able to parse Post parameters
+app.use(express.urlencoded({extended:true})); //to be able to parse Post parameters 
 
+// Sends user straight to login. Most aesthetic for URL.
 app.get('/', async (req, res) => {
-  const genres = ['Action', 'Horror', 'Thriller', 'Western', 'Science Fiction', 'Drama', 'Romance',
+    res.redirect('/login');
+})
+
+app.get('/login', async (req, res) => {
+    res.render('index.ejs', {'css': 'login'});
+})
+
+app.get('/create-account', async (req, res) => {
+    res.render('createAccount.ejs', {'css': 'login'});
+})
+
+app.get('/home', async (req, res) => {
+    const genres = ['Action', 'Horror', 'Thriller', 'Western', 'Science Fiction', 'Drama', 'Romance',
                     'Comedy', 'Fantasy', 'Animation', 'Documentary', 'Mystery', 'Teen'];
     const genreList = [];
-
 
     for (let i = 0; i < 4; i++) {
         let number = getRandomNumFromLength(genres.length);
@@ -31,7 +43,6 @@ app.get('/', async (req, res) => {
         } else {
             i--;
         }
-
     }
 
     let movieData = await fetchMovieData();
@@ -41,8 +52,7 @@ app.get('/', async (req, res) => {
         genreMovies.push(await fetchMoviesFromGenres(genre));
     }
 
-
-    res.render('index.ejs', {'css': 'main',
+    res.render('home.ejs', {'css': 'main',
         'bannerImg': movieData.bannerUrl,
         'movieDescription': movieData.description,
         'movieTitle': movieData.title,
@@ -50,7 +60,29 @@ app.get('/', async (req, res) => {
         'trendingMoviesImg': movieData.trendingMoviesImg,
         'genreList': genreList,
         'genreMovies': genreMovies});
+
 })
+
+app.get('/settings', (req,res) => {
+  res.render('userSettings.ejs', {'css': 'settings'});
+})
+
+app.listen(3000, () => {
+    console.log("server started");
+})
+
+async function executeSQL(query, params) {
+  return new Promise((resolve, reject) => {
+    dbAccess.query(query, params, (err, rows, fields) => {
+      if (err) throw err;
+      resolve(rows);
+    });
+  });
+}
+
+function getRandomNumFromLength(size) {
+    return Math.floor(Math.random() * size);
+}
 
 async function fetchMovieData() {
     try {
@@ -58,7 +90,7 @@ async function fetchMovieData() {
         const response = await fetch(url, apiOptions)
         const data = await  response.json();
         const index = getRandomNumFromLength(data.results.length)
-        const banner = await getBestBanner(data.results[index].id);
+        const banner = data.results[index].backdrop_path;
         const trendingMovies = [];
         const trendingMoviesImg = [];
         data.results.forEach((movie) => {
@@ -77,27 +109,6 @@ async function fetchMovieData() {
     }
 }
 
-async function fetchBanner() {
-
-    try {
-        const url = 'https://api.themoviedb.org/3/movie/popular';
-        const response = await fetch(url, apiOptions)
-        const data = await  response.json();
-        return 'https://image.tmdb.org/t/p/original' + data.results[0].backdrop_path;
-    } catch (err) {
-        console.error("APIerror:" + err);
-    }
-
-}
-
-function getRandomNumFromLength(size) {
-    return Math.floor(Math.random() * size);
-}
-
-app.get('/settings', (req,res) => {
-  res.render('userSettings.ejs', {'css': 'settings'});
-})
-
 async function fetchMoviesFromGenres(genre) {
     const url = `https://api.themoviedb.org/3/discover/movie?language=en-US&page=3&sort_by=popularity.desc&with_genres=${genre}`;
     const response = await fetch(url, apiOptions)
@@ -105,6 +116,74 @@ async function fetchMoviesFromGenres(genre) {
 
     return data.results;
 }
+
+// if testing the db table access and api info pull, uncomment the lines below.
+test();
+
+async function test() {
+  // the next 3 lines test database table access
+  console.log(await executeSQL("SELECT * FROM user"));
+  console.log(await executeSQL("SELECT * FROM list"));
+  console.log(await executeSQL("SELECT * FROM list_entry"));
+
+  // everything below grabs a movie from list_entry table and then calls tmdb api for info
+  let scifiListId = (await executeSQL(`SELECT list_id 
+                                       FROM list 
+                                       WHERE list_name = 'Scifi favs'`))[0].list_id;
+
+  let url = (await executeSQL(`SELECT title_id
+                                  FROM list_entry
+                                  WHERE list_id = ${scifiListId}
+                                  LIMIT 1`))[0].title_id;
+
+  let movieInfo = await (await fetch(url, apiOptions)).json();
+  console.log(movieInfo);
+}
+
+app.post('/create-account', async(req, res) =>{
+  const username = req.body.username;
+  const password = req.body.password;
+
+  console.log("Username:" + username);
+  console.log("Password:" + password);
+})
+
+app.post("/login", async(req, res) => {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log("username: " + username);
+  console.log("password: " + password);
+  let hashedPwd = "";
+  
+  let sql = "SELECT * FROM user WHERE username = ?";
+  let rows = await executeSQL(sql, [username]);
+
+  if (rows === null || rows.length === 0) {
+    req.session.authenticated = false;
+    res.redirect('/');
+    return;
+  }
+  
+  if(rows.length > 0){
+    hashedPwd = rows[0].password;
+  }
+
+  let passwordMatch = await bcrypt.compare(password, hashedPwd);
+  console.log("passwordMatch:" + passwordMatch + hashedPwd + password); 
+  
+  if(passwordMatch){
+    req.session.authenticated = true;
+    req.session.loginError = true;
+    res.redirect('/home');
+  } else {
+    req.session.authenticated = false;
+    res.render('index.ejs',{"loginError": true});
+  }
+});
+
+app.get("/home", isAuthenticated, (req, res) => {
+    res.redirect("/");
+});
 
 //functions
 function isAuthenticated(req, res, next){
@@ -114,78 +193,3 @@ function isAuthenticated(req, res, next){
     next();
   }
 }
-
-app.get("/home", isAuthenticated, (req, res) => {
-    res.redirect("home");
-});
-
-app.post('/login', async (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (username === 'user1' && password === 'secret'){
-    res.session.authenticated = true;
-    res.redirect('/home');
-  } else {
-    res.render('index.ejs', {'css': 'login', 'loginError': true});
-  }
-});
-
-app.post("/", async(req, res) => {
-  let username = req.body.username;
-  let password = req.body.password;
-  console.log("username: " + username);
-  console.log("password: " + password);
-  let hashedPwd = await bcrypt.hash(password, 10);
-
-  let sql = "SELECT * FROM user WHERE username = ?";
-  let rows = await executeSQL(sql, [username]);
-
-  if (rows === null || rows.length === 0) {
-    req.session.authenticated = false;
-    res.render("index", { "loginError": true });
-    return;
-  }
-
-  if(rows.length > 0){
-    hashedPwd = rows[0].password;
-  }
-
-  let passwordMatch = await bcrypt.compare(password, hashedPwd);
-  console.log("passwordMatch:" + passwordMatch);
-
-  if(passwordMatch){
-    req.session.authenticated = true;
-    res.render("home");
-  } else {
-    req.session.authenticated = false;
-    res.render("index",{"loginError": true});
-  }
-});
-
-app.listen(3000, () => {
-  console.log("server started");
-})
-
-// if testing the db table access and api info pull, uncomment the lines below.
-//test();
-//
-//async function test() {
-//  // the next 3 lines test database table access
-//  console.log(await executeSQL("SELECT * FROM user"));
-//  console.log(await executeSQL("SELECT * FROM list"));
-//  console.log(await executeSQL("SELECT * FROM list_entry"));
-//
-//  // everything below grabs a movie from list_entry table and then calls tmdb api for info
-//  let scifiListId = (await executeSQL(`SELECT list_id
-//                                       FROM list
-//                                       WHERE list_name = 'Scifi favs'`))[0].list_id;
-//
-//  let url = (await executeSQL(`SELECT title_id
-//                                  FROM list_entry
-//                                  WHERE list_id = ${scifiListId}
-//                                  LIMIT 1`))[0].title_id;
-//
-//  let movieInfo = await (await fetch(url, apiOptions)).json();
-//  console.log(movieInfo);
-//}
---
+++ src/public/js/carousel.js
@@ -0,0 +1,81 @@
+function createCarousel(elementID) {
+    let currentIndex = 0;
+    // TODO: Temp hard-code. SIZE references how many cells fit on page. Will need to do dynamically.
+    const SIZE = 5;
+    // TODO: Temp hard-code. Lists are yet to be implemented
+    const listLength = 3;
+    const carousel = document.querySelector(`#${elementID}`);
+    const carouselItems = document.querySelectorAll(`#${elementID} .carousel-item`);
+
+    if (listLength < SIZE) {
+        document.querySelector("#b2").style.display = 'none';
+    }
+
+    function prevSlide() {
+        if (currentIndex !== 0) {
+            currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+            updateCarousel();
+
+            // Scuffed way of changing nextSlide button display
+            let id = this.id;
+            id = id.substring(1);
+            id = parseInt(id) + 1;
+            id = 'b' + id;
+            document.querySelector(`#${id}`).style.display = 'block';
+
+            if (currentIndex === 0) {
+                this.style.display = 'none';
+            }
+        }
+    }
+
+    function nextSlide() {
+        if (currentIndex !== carouselItems.length/SIZE){
+            currentIndex = (currentIndex + 1) % carouselItems.length;
+            updateCarousel();
+            if (currentIndex === carouselItems.length/SIZE) {
+                this.style.display = 'none';
+            }
+        }
+
+        // Scuffed way of changing prevSlide button display
+        if (currentIndex !== 0) {
+            let id = this.id;
+            id = id.substring(1);
+            id = parseInt(id) - 1;
+            id = 'b' + id;
+            document.querySelector(`#${id}`).style.display = 'block';
+        }
+    }
+
+    function updateCarousel() {
+        carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    return { prevSlide, nextSlide };
+}
+
+    const listCarousel = createCarousel('list-carousel');
+    const trendingCarousel = createCarousel('Trending-carousel');
+    const genre0Carousel = createCarousel('genre0-carousel');
+    const genre1Carousel = createCarousel('genre1-carousel');
+    const genre2Carousel = createCarousel('genre2-carousel');
+    const genre3Carousel = createCarousel('genre3-carousel');
+
+    document.querySelector('#b1').addEventListener('click', listCarousel.prevSlide);
+    document.querySelector('#b2').addEventListener('click', listCarousel.nextSlide);
+
+    document.querySelector('#b3').addEventListener('click', trendingCarousel.prevSlide);
+    document.querySelector('#b4').addEventListener('click', trendingCarousel.nextSlide);
+
+    document.querySelector('#b5').addEventListener('click', genre0Carousel.prevSlide);
+    document.querySelector('#b6').addEventListener('click', genre0Carousel.nextSlide);
+
+    document.querySelector('#b7').addEventListener('click', genre1Carousel.prevSlide);
+    document.querySelector('#b8').addEventListener('click', genre1Carousel.nextSlide);
+
+    document.querySelector('#b9').addEventListener('click', genre2Carousel.prevSlide);
+    document.querySelector('#b10').addEventListener('click', genre2Carousel.nextSlide);
+
+    document.querySelector('#b11').addEventListener('click', genre3Carousel.prevSlide);
+    document.querySelector('#b12').addEventListener('click', genre3Carousel.nextSlide);
