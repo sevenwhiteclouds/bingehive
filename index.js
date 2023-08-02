6595ada7eae023b449c3ec0cf7366729aefc6b76
sevenwhiteclouds <kemaldonado@csumb.edu>
2023-08-02 09:02:56 -0700

    api and db test works

--- index.js
+++ index.js
@@ -10,19 +10,25 @@ const apiOptions = {
   }
 };
 
-testDb();
+// if testing the db table access and api info pull, uncomment the lines below.
+test();
 
-async function testDb() {
+async function test() {
+  // the next 3 lines test database table access
   console.log(await executeSQL("SELECT * FROM user"));
   console.log(await executeSQL("SELECT * FROM list"));
   console.log(await executeSQL("SELECT * FROM list_entry"));
-}
 
-async function executeSQL(query, params) {
-  return new Promise((resolve, reject) => {
-    pool.query(query, params, (err, rows, fields) => {
-      if (err) throw err;
-      resolve(rows);
-    });
-  });
+  // everything below grabs a movie from list_entry table and then calls tmdb api for info
+  let scifiListId = (await executeSQL(`SELECT list_id
+                                       FROM list
+                                       WHERE list_name = 'Scifi favs'`))[0].list_id;
+
+  let url = (await executeSQL(`SELECT title_id
+                                  FROM list_entry
+                                  WHERE list_id = ${scifiListId}
+                                  LIMIT 1`))[0].title_id;
+
+  let movieInfo = await (await fetch(url, apiOptions)).json();
+  console.log(movieInfo);
 }
